ea4d1edaf3d19b804d7ab7fa75428bb34e00cb28
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-10 11:35:16 +0300

    no commit message given

--- config_homedir.sh
+++ config_homedir.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -e
 touch ~/.hushlogin
 cat sourceme >> ~/.bashrc
-echo "source ~/.venv/bin/activate" >> ~/.bashrc
-echo "export PATH=$PATH:$HOME/install/aws/bin" >> ~/.bashrc
-ecoh "complete -C aws_completer aws" >> ~/.bashrc
