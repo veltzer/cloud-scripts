3aeebcd46f3b2ae1cd1adbde29fc420904f3ced0
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-10 12:51:34 +0300

    no commit message given

--- update_everything.sh
+++ update_everything.sh
@@ -2,6 +2,7 @@
 # get ridd of the ubuntu "pro" commericals
 sudo rm -f /etc/apt/apt.conf.d/20apt-esm-hook.conf
 sudo touch /etc/apt/apt.conf.d/20apt-esm-hook.conf
+# sudo pro config set apt_news=false
 # update and upgrade everything
 sudo apt update
 sudo apt dist-upgrade -y
